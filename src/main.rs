9b70ccca6924df21a2d58a9876b135c1bb397f97
kathukyabrian <briankathukya2000@gmail.com>
2026-04-15 18:01:07 +0300

    added logic to create service configuration

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ pub mod env;
 use tera::{Context, Tera};
 
 fn main() {
-    println!(r#"
+    println!(
+        r#"
         ██╗  ██╗██╗   ██╗██████╗ ███████╗ ██████╗ ███████╗███╗   ██╗
         ██║ ██╔╝██║   ██║██╔══██╗██╔════╝██╔════╝ ██╔════╝████╗  ██║
         █████╔╝ ██║   ██║██████╔╝█████╗  ██║  ███╗█████╗  ██╔██╗ ██║
@@ -12,32 +13,57 @@ fn main() {
         ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝
 
                 Generate Kubernetes YAML like a boss
-    "#);
+    "#
+    );
 
     // get user input
     // name
     // image
+    // port
+    // service type - ClusterIP, NodePort, LoadBalancer
 
+    let mut tera = load_templates();
+
+    let deployment = generate_deployment(&mut tera, "mobireg", "mobi", "3000");
+    let service = generate_service(&mut tera, "mobireg", "NodePort", "3000");
+
+    println!("{}", deployment);
+    println!("---");
+    println!("{}", service);
+}
+
+fn load_templates() -> Tera {
     let mut tera = Tera::default();
     tera.add_template_file("deployment.yaml.tera", Some("deployment"))
         .unwrap();
+    tera.add_template_file("service.yaml.tera", Some("service"))
+        .unwrap();
+    tera.add_template_file("ingress.yaml.tera", Some("ingress"))
+        .unwrap();
+    tera.add_template_file("configmap.yaml.tera", Some("configmap"))
+        .unwrap();
 
-    let deployment = generate_deployment(&mut tera, "mobireg", "mobi");
-
-    println!("{}", deployment);
+    tera
 }
 
-fn generate_deployment(tera: &Tera, name: &str, image: &str) -> String {
+fn generate_deployment(tera: &Tera, name: &str, image: &str, port: &str) -> String {
     let mut context = Context::new();
     context.insert("name", name);
     context.insert("image", image);
+    context.insert("port", port);
     let rendered = tera.render("deployment", &context).unwrap();
     rendered
 }
 
-// fn generateService() -> &str {
-//
-// }
+fn generate_service(tera: &Tera, name: &str, service_type: &str, port: &str) -> String {
+    let mut context = Context::new();
+    context.insert("name", name);
+    context.insert("service_type", service_type);
+    context.insert("port", port);
+
+    let rendered = tera.render("service", &context).unwrap();
+    rendered
+}
 //
 // fn generateIngress() {
 //
